24e92a0e5c9e55072ee3c10a3634595c0a158b56
alamin khan <alaminuixd@gmail.com>
2023-12-02 00:34:36 +0600

    commited 1st module function capitalize

---
+++ text-effects.js
@@ -0,0 +1,9 @@
+// Capitalize each first letter of each letter in a string
+function capitalize(text) {
+  let words = text.split(" ");
+  words = words.map((word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  });
+  return words.join(" ");
+}
+console.log(capitalize("hello how are you doing today?"));
