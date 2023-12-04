75049cc76d95e20dde1d16b127bd6f818c53e5a6
alamin khan <alaminuixd@gmail.com>
2023-12-04 15:34:20 +0600

    added constructor object 'Person' and a new function

--- cont.js
+++ cont.js
@@ -1,3 +1,20 @@
 /* 
 some functionality will be here
 */
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.details = function () {
+    return `My name is ${this.name} and I am ${this.age} years old`;
+  };
+}
+function myNew(cons) {
+  const obj = {};
+  Object.setPrototypeOf(obj, cons.prototype);
+  const argArray = Array.from(arguments);
+  cons.apply(obj, argArray.slice(1));
+  return obj;
+}
+
+const p1 = myNew(Person, "Al Amin", 37);
+console.log(p1.details());
