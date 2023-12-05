fa6221e0f6d6b0c1fef51d886334518b7a771e40
alamin khan <alaminuixd@gmail.com>
2023-12-05 13:00:06 +0600

    Commited two new constructor Teacher and Car

--- cont.js
+++ cont.js
@@ -1,13 +1,6 @@
 /* 
 some functionality will be here
 */
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
-  this.details = function () {
-    return `My name is ${this.name} and I am ${this.age} years old`;
-  };
-}
 function myNew(cons) {
   const obj = {};
   Object.setPrototypeOf(obj, cons.prototype);
@@ -15,6 +8,36 @@ function myNew(cons) {
   cons.apply(obj, argArray.slice(1));
   return obj;
 }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.details = function () {
+    return `My name is ${this.name} and I am ${this.age} years old`;
+  };
+}
+
+function Teacher(name, department, awardNum) {
+  this.name = name;
+  this.department = department;
+  this.awardNum = awardNum;
+  this.details = function () {
+    return `Teacher: ${this.name} \nFrom: ${this.department} department \nAward received: ${this.awardNum}`;
+  };
+}
+
+function Car(name, modle, wheel, seats, fuel) {
+  this.name = name;
+  this.modle = modle;
+  this.wheel = wheel;
+  this.seats = seats;
+  this.fuel = fuel;
+  this.details = function () {
+    return `Car name: ${this.name}. modle: ${this.modle}. Numbers of wheels: ${this.wheel}. Number of seats: ${this.seats}. Fuel type: ${this.fuel}`;
+  };
+}
 
 const p1 = myNew(Person, "Al Amin", 37);
 console.log(p1.details());
+
+const t1 = myNew(Teacher, "Atiar Rahman", "Science", 3);
+console.log(t1.details());
